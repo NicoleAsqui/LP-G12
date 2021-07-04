f32a811a7c02fdb6b14babd286e7042fd411c237
NicoleAsqui <nasqui@espol.edu.ec>
2021-07-04 14:11:29 -0500

    correciones

--- AnalizadorLexico/main.py
+++
@@ -1,7 +0,0 @@
-from mainFrame import MainFrame
-from tkinter import Tk
-
-ventanaPrincipal=Tk()
-analizador=MainFrame(ventanaPrincipal)
-ventanaPrincipal.iconbitmap(r"C:\Users\bllv1\OneDrive - Escuela Superior Politécnica del Litoral\1S-2021\Lenguajes de programacion\p1p\LP-G12\archivos\javaScript.ico")
-ventanaPrincipal.mainloop()
